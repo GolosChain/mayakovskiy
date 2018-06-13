90a4911cf177357ea06fd166a20060a4bb497652
Oleg <oleg.pav.m@gmail.com>
2018-06-13 14:33:46 +0300

    Logger.

---
+++ src/core/Logger.js
@@ -0,0 +1,31 @@
+require('colors');
+const moment = require('moment');
+
+class Logger {
+    static log() {
+        this._log('[log]', arguments, 'grey');
+    }
+
+    static info() {
+        this._log('[info]', arguments, 'blue');
+    }
+
+    static error() {
+        this._log('[error]', arguments, 'red');
+    }
+
+    static _log(prefix, data, color) {
+        console.log.apply(console, [
+            this._now(),
+            `<${process.pid}>`,
+            prefix[color],
+            ...data,
+        ]);
+    }
+
+    static _now() {
+        return moment().format('YYYY-MM-DD HH:ss');
+    }
+}
+
+module.exports = Logger;
