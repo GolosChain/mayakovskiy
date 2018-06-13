8ade02d9e07c275608c65015f82b8f621c94d8de
Oleg <oleg.pav.m@gmail.com>
2018-06-13 15:09:34 +0300

    Main service and customer service stub.

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,45 @@
-class Main {
-    run() {
-        // TODO logic
+const logger = require('./core/Logger');
+const AbstractService = require('./core/AbstractService');
+const OfficialCustomerService = require('./officialСustomerService/Main');
+
+class Main extends AbstractService {
+    constructor() {
+        super();
+
+        // TODO configuration
+
+        this.services = [
+            new OfficialCustomerService(),
+            // any another services here
+        ];
+
+        process.on('exit', this.stop);
+        process.on('SIGINT', this.stop);
+    }
+
+    start() {
+        logger.info('Start services...');
+
+        this.services.forEach(service => {
+            logger.info(`\tStart ${service.constructor.name}...`);
+            service.start();
+            logger.info(`\tThe ${service.constructor.name} done!`);
+        });
+
+        logger.info('Start services done!');
+    }
+
+    stop() {
+        logger.info('Cleanup...');
+
+        this.services.forEach(service => {
+            logger.info(`\tStop ${service.constructor.name}...`);
+            service.stop();
+            logger.info(`\tThe ${service.constructor.name} done!`);
+        });
+
+        logger.info('Cleanup done!');
     }
 }
 
-new Main().run();
+new Main().start();
---
+++ src/officialСustomerService/Main.js
@@ -0,0 +1,17 @@
+const logger = require('../core/Logger');
+const BlockChain = require('../core/BlockChain');
+const AbstractService = require('../core/AbstractService');
+
+class OfficialCustomerService extends AbstractService {
+    constructor() {
+        super();
+
+        // TODO logic
+    }
+
+    start() {
+        // TODO logic
+    }
+}
+
+module.exports = OfficialCustomerService;
