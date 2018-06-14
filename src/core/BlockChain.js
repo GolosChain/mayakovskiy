772e24d41b468198be991a580587110962fce680
Oleg <oleg.pav.m@gmail.com>
2018-06-14 20:24:01 +0300

    BlockChain connector (draft).

--- src/core/BlockChain.js
+++ src/core/BlockChain.js
@@ -1,9 +1,73 @@
 const W3CWebSocket = require('websocket').w3cwebsocket;
+const logger = require('./Logger');
 
 class BlockChain {
-    // TODO rich high-level api for services
+    static async connect() {
+        this.listeners = [];
 
-    eachBlock(callback) {
-        // TODO -
+        return new Promise((resolve, reject) => {
+            this.socket = new W3CWebSocket('wss://ws.golos.io');
+
+            this.socket.onerror = error => {
+                logger.error(`BlockChain socket error - ${error}`);
+                reject();
+            };
+
+            this.socket.onopen = () => {
+                logger.info('BlockChain socket connection established.');
+                resolve();
+            };
+
+            this.socket.onclose = () => {
+                this._disconnectCallback && this._disconnectCallback();
+                logger.info('BlockChain socket disconnected.');
+            };
+
+            this.socket.onmessage = data => {
+                this.listeners.forEach(listener => {
+                    listener && listener(data);
+                });
+            };
+        });
+    }
+
+    static async disconnect() {
+        this.listeners = [];
+
+        return new Promise((resolve, reject) => {
+            try {
+                this._disconnectCallback = () => {
+                    resolve();
+                    delete this._disconnectCallback;
+                };
+
+                this.socket.close();
+            } catch (error) {
+                delete this._disconnectCallback;
+                reject();
+            }
+        });
+    }
+
+    static eachBlockRealTime(callback) {
+        (function loop() {
+            // TODO -
+
+            setImmediate(loop);
+        }.bind(this)());
+    }
+
+    static eachTransactionRealTime(callback) {
+        this.eachBlockRealTime(data => {
+            // TODO filtrate only blocks with transactions
+        });
+    }
+
+    static eachPostRealTime(callback) {
+        this.eachTransactionRealTime(data => {
+            // TODO filtrate only blocks with posts
+        });
     }
 }
+
+module.exports = BlockChain;
