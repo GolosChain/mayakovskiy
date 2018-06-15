2851d0bbb1cbf882bffbb678e90c87fba846e2a2
Oleg <oleg.pav.m@gmail.com>
2018-06-15 11:55:48 +0300

    BlockChain message handler.

--- src/core/BlockChain.js
+++ src/core/BlockChain.js
@@ -3,36 +3,17 @@ const logger = require('./Logger');
 
 class BlockChain {
     static async connect() {
-        this.listeners = [];
+        this._resetListenersMeta();
 
         return new Promise((resolve, reject) => {
             this.socket = new W3CWebSocket('wss://ws.golos.io');
-
-            this.socket.onerror = error => {
-                logger.error(`BlockChain socket error - ${error}`);
-                reject();
-            };
-
-            this.socket.onopen = () => {
-                logger.info('BlockChain socket connection established.');
-                resolve();
-            };
-
-            this.socket.onclose = () => {
-                this._disconnectCallback && this._disconnectCallback();
-                logger.info('BlockChain socket disconnected.');
-            };
-
-            this.socket.onmessage = data => {
-                this.listeners.forEach(listener => {
-                    listener && listener(data);
-                });
-            };
+            this._makeEventHandlers(resolve, reject);
+            this.socket.onmessage = this._messageHandler;
         });
     }
 
     static async disconnect() {
-        this.listeners = [];
+        this._resetListenersMeta();
 
         return new Promise((resolve, reject) => {
             try {
@@ -42,6 +23,7 @@ class BlockChain {
                 };
 
                 this.socket.close();
+                delete this.socket;
             } catch (error) {
                 delete this._disconnectCallback;
                 reject();
@@ -67,6 +49,64 @@ class BlockChain {
         this.eachTransactionRealTime(data => {
             // TODO filtrate only blocks with posts
         });
+    }
+
+    static _makeEventHandlers(resolve, reject) {
+        this.socket.onerror = error => {
+            logger.error(`BlockChain socket error - ${error}`);
+            reject();
+        };
+
+        this.socket.onopen = () => {
+            logger.info('BlockChain socket connection established.');
+            resolve();
+        };
+
+        this.socket.onclose = () => {
+            this._disconnectCallback && this._disconnectCallback();
+            logger.info('BlockChain socket disconnected.');
+        };
+    }
+
+    static _messageHandler(rawData) {
+        let data = this._parseMessageData(rawData);
+
+        if (!data) {
+            return;
+        }
+
+        if (this.listeners[data.id]) {
+            this._callListener(this.listeners[data.id], data);
+            delete this.listeners[data.id];
+        } else {
+            logger.error('Unknown BlockChain socket message id.');
+        }
+    }
+
+    static _parseMessageData(rawData) {
+        try {
+            return JSON.parse(rawData);
+        } catch (error) {
+            logger.error(`Invalid BlockChain socket data - ${error}`);
+            return null;
+        }
+    }
+
+    static _callListener(listener, data) {
+        if (typeof listener === 'function') {
+            listener(data);
+        } else {
+            logger.error(`Invalid BlockChain message handler.`);
+        }
+    }
+
+    static makeListenerId() {
+        return ++this.lastListenerId;
+    }
+
+    static _resetListenersMeta() {
+        this.listeners = {};
+        this.lastListenerId = 0;
     }
 }
 
