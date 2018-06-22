00ca62ebff10fb4e3411faf6f227534fdb25f1f1
Oleg <oleg.pav.m@gmail.com>
2018-06-22 15:44:02 +0300

    BlockSubscribe draft.

--- src/core/service/BlockSubscribe.js
+++ src/core/service/BlockSubscribe.js
@@ -1,26 +1,79 @@
+const WebSocket = require('ws');
 const env = require('../Env');
 const logger = require('../Logger');
 const BasicService = require('../service/Basic');
 
+// TODO remove after golos-js implement this methods
+const MAGIC_SUBSCRIBE_CALL =
+    '{"id":1,"jsonrpc":"2.0","method":"call","params":["database_api","set_block_applied_callback",[0]]}';
+
 class BlockSubscribe extends BasicService {
-    static getBlockNum(blockHash) {
+    constructor() {
+        super();
+
+        this._alive = false;
+    }
+
+    static extractBlockNum(blockHash) {
         return parseInt(blockHash.slice(0, 8), 16);
     }
 
     async start(callback) {
-        // TODO -
+        this._socket = new WebSocket(env.BLOCKCHAIN_NODE_ADDRESS);
+
+        this._makeSocketHandlers();
+        this._startSocketWatchDog();
     }
 
     async stop() {
-        // TODO -
+        this._socket.terminate();
     }
 
     async blockBy(blockNum) {
         // TODO -
     }
 
-    async retry() {
-        // TODO -
+    _makeSocketHandlers() {
+        this._socket.on('error', this._handleError);
+        this._socket.on('message', raw => {
+            let response;
+
+            this._alive = true;
+
+            try {
+                response = JSON.parse(raw.data);
+            } catch (error) {
+                this._handleError(error);
+                return;
+            }
+
+            if (!response.result) {
+                this._handleError('Empty message result');
+                return;
+            }
+
+            return callback(response.result);
+        });
+        this._socket.on('open', () => {
+            logger.info('BlockSubscribe websocket connection established.');
+            this._socket.send(MAGIC_SUBSCRIBE_CALL);
+        });
+    }
+
+    _startSocketWatchDog() {
+        const dog = setInterval(() => {
+            if (!this._alive) {
+                clearInterval(dog);
+                this._handleError('Request timeout');
+            }
+
+            this._alive = false;
+        }, env.BLOCKCHAIN_SUBSCRIBE_TIMEOUT / 2);
+    }
+
+    _handleError(error) {
+        logger.error(`BlockSubscribe websocket error - ${error}`);
+        process.exit(1);
     }
 }
 
