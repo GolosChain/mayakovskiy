0e8a91e68638a9597612b0689a1200b992e53b56
IvanBlacky <tcherivan@gmail.com>
2018-12-24 13:55:31 +0300

    #6 Feat: add post body length validation support

--- src/Env.js
+++ src/Env.js
@@ -7,6 +7,9 @@ module.exports = {
     GLS_MIN_GOLOS_POWER: Number(env.GLS_MIN_GOLOS_POWER) || 0,
     GLS_PROHIBITED_TAGS: env.GLS_PROHIBITED_TAGS || 'goldvoice',
     GLS_GOLOS_APP_NAME: env.GLS_GOLOS_APP_NAME || 'golos.io/0.1',
+    // принимаемые значения: 'auto' (по умолчанию) ,'manual'
+    GLS_PLANNER_MODE: env.GLS_PLANNER_MODE || 'auto',
+    GLS_MIN_POST_LENGTH: Number(env.GLS_MIN_POST_LENGTH) || 0,
 };
 
 if (module.exports.GLS_PROHIBITED_TAGS === 'false') {
